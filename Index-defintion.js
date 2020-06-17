cf908ce44551c414d9b9be2ec0ce9e75c1b2cba1
v_zhtaozhou(周涛) <v_zhtaozhou@tencent.com>
2020-06-17 20:12:31 +0800

    ...

--- Index-defintion.js
+++ Index-defintion.js
@@ -21,21 +21,38 @@ define(function Header({ name }) {
 });
 
 define(function Menu() {
+	let items = []; // []string
+	let current = 0; // uint
+	// const
+
+	const _fz0g = (ctx) => items.length > 3;
+	const _zf44 = (ctx, fn) =>
+		items.map((item, idx) => fn({ item, idx, ...ctx }));
+	const _fkl0 = (ctx) => {
+		return { tags: { current: current === ctx.idx } };
+	};
+	const _xx89 = (ctx) => {
+		return { tx: ctx.item };
+	};
+
+	const _fjp4 = () => {};
+
 	return {
 		onDesdroyed: () => {},
 		children: [
 			["ul"],
-			["ul/@if", "items.length > 3"],
-			["ul/@else"],
-			["ul/@if/@for", "items", "item, idx"],
-			["ul/@else/span", "..."],
-			["ul/@if/@for/li", "tag = idx == current ? 'current' : ''"],
-			["ul/@if/@for/li/span", ".label", "item"],
+			["ul/@if", _fz0g],
+			["ul/@else", _fz0g],
+			["ul/@if/@for", _zf44],
+			["ul/@else/span", _fjp4],
+			["ul/@if/@for/li", _fkl0],
+			["ul/@if/@for/li/span", _xx89],
 		],
 		modifiers: {
-			"ul/@if": ["condition", "items.length"],
-			"ul/@if/@for": ["iteration", "items"],
-			"ul/@if/@for/li": ["access", "item"],
+			"ul/@if": { "items.length": true },
+			"ul/@if/@for": { items: true },
+			"ul/@if/@for/li": { current: true },
+			"ul/@if/@for/li/span": { item: true },
 		},
 	};
 });
